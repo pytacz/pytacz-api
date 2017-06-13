872c1ed5629d95601a98cc3248ffa6d25ada30fd
Gurlox <gurlox@wp.pl>
2017-06-13 22:30:49 +0000

    Add User Entity

---
+++ src/ApiBundle/Entity/User.php
@@ -0,0 +1,406 @@
+<?php
+
+namespace ApiBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
+
+/**
+ * Users
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="ApiBundle\Repository\UserRepository")
+ */
+class User implements AdvancedUserInterface, \Serializable
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=20, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=60, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=64)
+     */
+    private $password;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="register_date", type="datetime")
+     */
+    private $registerDate;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    private $isActive;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_date", type="datetime", nullable=true)
+     */
+    private $lastDate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="last_ip", type="string", length=15)
+     */
+    private $lastIp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="register_ip", type="string", length=15)
+     */
+    private $registerIp;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=50, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="register_hash", type="string", length=30, nullable=true)
+     */
+    private $registerHash;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     *
+     * @return Users
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Users
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Users
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set registerDate
+     *
+     * @param \DateTime $registerDate
+     *
+     * @return Users
+     */
+    public function setRegisterDate($registerDate)
+    {
+        $this->registerDate = $registerDate;
+
+        return $this;
+    }
+
+    /**
+     * Get registerDate
+     *
+     * @return \DateTime
+     */
+    public function getRegisterDate()
+    {
+        return $this->registerDate;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return Users
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return bool
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+
+    /**
+     * Set lastDate
+     *
+     * @param \DateTime $lastDate
+     *
+     * @return Users
+     */
+    public function setLastDate($lastDate)
+    {
+        $this->lastDate = $lastDate;
+
+        return $this;
+    }
+
+    /**
+     * Get lastDate
+     *
+     * @return \DateTime
+     */
+    public function getLastDate()
+    {
+        return $this->lastDate;
+    }
+
+    /**
+     * Set lastIp
+     *
+     * @param string $lastIp
+     *
+     * @return Users
+     */
+    public function setLastIp($lastIp)
+    {
+        $this->lastIp = $lastIp;
+
+        return $this;
+    }
+
+    /**
+     * Get lastIp
+     *
+     * @return string
+     */
+    public function getLastIp()
+    {
+        return $this->lastIp;
+    }
+
+    /**
+     * Set registerIp
+     *
+     * @param string $registerIp
+     *
+     * @return Users
+     */
+    public function setRegisterIp($registerIp)
+    {
+        $this->registerIp = $registerIp;
+
+        return $this;
+    }
+
+    /**
+     * Get registerIp
+     *
+     * @return string
+     */
+    public function getRegisterIp()
+    {
+        return $this->registerIp;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Users
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set registerHash
+     *
+     * @param string $registerHash
+     *
+     * @return Users
+     */
+    public function setRegisterHash($registerHash)
+    {
+        $this->registerHash = $registerHash;
+
+        return $this;
+    }
+
+    /**
+     * Get registerHash
+     *
+     * @return string
+     */
+    public function getRegisterHash()
+    {
+        return $this->registerHash;
+    }
+
+    public function isAccountNonExpired()
+    {
+        return true;
+    }
+
+    public function isAccountNonLocked()
+    {
+        return true;
+    }
+
+    public function isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    public function isEnabled()
+    {
+        return $this->isActive;
+    }
+
+    public function getRoles()
+    {
+        return array('ROLE_USER');
+    }
+
+    public function eraseCredentials()
+    {
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    /** @see \Serializable::serialize() */
+    public function serialize()
+    {
+        return serialize(array(
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive
+            // see section on salt below
+            // $this->salt,
+        ));
+    }
+
+    /** @see \Serializable::unserialize() */
+    public function unserialize($serialized)
+    {
+        list (
+            $this->id,
+            $this->username,
+            $this->password,
+            $this->isActive
+            // see section on salt below
+            // $this->salt
+            ) = unserialize($serialized);
+    }
+}
+
